f7f8cbdd986435ec9e7e79305c641fbe4bae192e
Sam <sam@malpass.me.uk>
2018-04-08 17:26:24 +0100

    Add variables

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/MainMenu.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/MainMenu.java
@@ -3,8 +3,12 @@ package uk.me.malpass.android_space_invaders_game;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainMenu extends Activity {
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
+public class MainMenu extends Activity {
+    SignIn signIn;
+    SpaceInvadersView SpaceInvadersView;
+    public static GoogleSignInAccount account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
