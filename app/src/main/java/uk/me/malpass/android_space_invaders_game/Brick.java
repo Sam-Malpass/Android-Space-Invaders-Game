b62189cd4eab5dd4e42e7edc5e4a960e9d0fd3e0
Sam <sam@malpass.me.uk>
2018-04-08 17:44:07 +0100

    Add constructor

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/Brick.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/Brick.java
@@ -9,4 +9,13 @@ import android.graphics.RectF;
 public class Brick {
     public RectF rect;
     public boolean visibile;
+    public Brick(int row, int column, int shelterNumber, int screenX, int screenY) {
+        int width = screenX / 90;
+        int height = screenY / 40;
+        visibile = true;
+        int blockPadding = 0;
+        int shelterPaddig = screenX / 9;
+        int startHeight = screenY - (screenY / 8 * 2);
+        rect = new RectF(column * width + blockPadding + (shelterPaddig * shelterNumber) + shelterPaddig + shelterPaddig * shelterNumber, row * height + blockPadding + startHeight, column * width + width - blockPadding + (shelterPaddig * shelterNumber) + shelterPaddig + shelterPaddig * shelterNumber, row * height + height - blockPadding + startHeight);
+    }
 }
