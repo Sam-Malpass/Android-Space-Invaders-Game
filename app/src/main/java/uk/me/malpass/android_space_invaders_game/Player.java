189b5c0e61ec768f133b20fb9713e20d81f70fef
Sam <sam@malpass.me.uk>
2018-04-08 17:41:00 +0100

    Add variables

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/Player.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/Player.java
@@ -1,8 +1,22 @@
 package uk.me.malpass.android_space_invaders_game;
 
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+
 /**
  * Created by sam on 08/04/2018.
  */
 
 public class Player {
+    RectF rect;
+    private Bitmap bitmap;
+    private float length;
+    private float height;
+    private float x;
+    private float y;
+    private float speed;
+    public final int STOPPED = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+    private int moving = STOPPED;
 }
