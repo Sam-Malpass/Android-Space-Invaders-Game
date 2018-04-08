3cc301a5683f95e45b0cae3408823a077cec6947
Sam <sam@malpass.me.uk>
2018-04-08 17:19:58 +0100

    Add signIn function

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
@@ -1,5 +1,8 @@
 package uk.me.malpass.android_space_invaders_game;
 
+import android.app.Activity;
+import android.content.Intent;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 
@@ -7,8 +10,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
  * Created by sam on 08/04/2018.
  */
 
-public class SignIn {
-    GoogleSignInClient mGoogleSignInClient;
+public class SignIn extends Activity {
+    private GoogleSignInClient mGoogleSignInClient;
     public static GoogleSignInAccount account;
     private static int RC_SIGN_IN = 100;
+
+    public void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 }
