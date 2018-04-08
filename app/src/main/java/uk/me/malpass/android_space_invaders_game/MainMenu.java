be0e9df0148848a12345f9b5873ce12887f63a6c
Sam <sam@malpass.me.uk>
2018-04-08 16:45:15 +0100

    Add project files

---
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/MainMenu.java
@@ -0,0 +1,13 @@
+package uk.me.malpass.android_space_invaders_game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
