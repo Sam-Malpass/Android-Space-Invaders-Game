72260d751ea3b258a513f9db3e16e8e12f5231b7
Sam <sam@malpass.me.uk>
2018-04-08 17:47:26 +0100

    Add update function

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/Invader.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/Invader.java
@@ -32,4 +32,16 @@ public abstract class Invader {
         }
         return false;
     }
+    public void update(long fps) {
+        if(moving == LEFT) {
+            x = x - speed / fps;
+        }
+        if(moving == RIGHT) {
+            x = x + speed / fps;
+        }
+        rect.top = y;
+        rect.bottom = y + height;
+        rect.left = x;
+        rect.right = x + length;
+    }
 }
