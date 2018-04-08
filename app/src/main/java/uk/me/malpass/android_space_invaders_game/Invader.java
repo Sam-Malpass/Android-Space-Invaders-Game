86c6fb502f682a967c5db68b4b70be2228e6afff
Sam <sam@malpass.me.uk>
2018-04-08 17:46:07 +0100

    Create Invader class

---
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/Invader.java
@@ -0,0 +1,13 @@
+package uk.me.malpass.android_space_invaders_game;
+
+import android.graphics.Bitmap;
+import android.graphics.RectF;
+
+import java.util.Random;
+
+/**
+ * Created by sam on 08/04/2018.
+ */
+
+public abstract class Invader {
+}
