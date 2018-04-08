95a61c6f2fdb7a3f6290b26dd385aa99afc498f7
Sam <sam@malpass.me.uk>
2018-04-08 17:20:41 +0100

    Add onActivityResult function

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
@@ -3,8 +3,10 @@ package uk.me.malpass.android_space_invaders_game;
 import android.app.Activity;
 import android.content.Intent;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.Task;
 
 /**
  * Created by sam on 08/04/2018.
@@ -18,5 +20,18 @@ public class SignIn extends Activity {
     public void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 }
