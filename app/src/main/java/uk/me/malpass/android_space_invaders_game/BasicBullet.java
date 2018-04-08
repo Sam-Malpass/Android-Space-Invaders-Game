1ad0b8d07c4ae2422236a4cf32609131990e1acd
Sam <sam@malpass.me.uk>
2018-04-08 17:59:15 +0100

    Add constructor

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/BasicBullet.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/BasicBullet.java
@@ -1,8 +1,16 @@
 package uk.me.malpass.android_space_invaders_game;
 
+import android.graphics.RectF;
+
 /**
  * Created by sam on 08/04/2018.
  */
 
 public class BasicBullet extends Bullet {
+    public BasicBullet(int screenY) {
+        height = screenY/20;
+        active = false;
+        rect = new RectF();
+        damage = 1;
+    }
 }
