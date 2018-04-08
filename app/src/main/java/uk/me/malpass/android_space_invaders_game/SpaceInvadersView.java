ce7f85e0b0793d6cdff6995d86cfe922caebb30b
Sam <sam@malpass.me.uk>
2018-04-08 17:33:07 +0100

    Create SpaceInvadersView class

---
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/SpaceInvadersView.java
@@ -0,0 +1,10 @@
+package uk.me.malpass.android_space_invaders_game;
+
+import android.view.SurfaceView;
+
+/**
+ * Created by sam on 08/04/2018.
+ */
+
+public class SpaceInvadersView  extends SurfaceView implements Runnable  {
+}
