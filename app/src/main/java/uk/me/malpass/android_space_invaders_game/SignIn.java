47bb4146e92992dce293d583b4b189b5950c4f01
Sam <sam@malpass.me.uk>
2018-04-08 17:24:35 +0100

    Remove comments

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
@@ -29,11 +29,7 @@ public class SignIn extends Activity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
@@ -43,8 +39,6 @@ public class SignIn extends Activity {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
         } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
         }
     }
