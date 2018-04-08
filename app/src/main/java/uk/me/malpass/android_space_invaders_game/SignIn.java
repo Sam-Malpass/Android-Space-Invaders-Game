480222c51a0f328235930e83bc48c0a5588b5ebd
Sam <sam@malpass.me.uk>
2018-04-08 17:18:02 +0100

    Add variables

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/SignIn.java
@@ -1,9 +1,14 @@
 package uk.me.malpass.android_space_invaders_game;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
 /**
  * Created by sam on 08/04/2018.
  */
 
 public class SignIn {
-    
+    GoogleSignInClient mGoogleSignInClient;
+    public static GoogleSignInAccount account;
+    private static int RC_SIGN_IN = 100;
 }
