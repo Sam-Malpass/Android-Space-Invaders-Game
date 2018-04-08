4374b7488b758776295e67d3d5614ba618af3ebc
Sam <sam@malpass.me.uk>
2018-04-08 17:43:44 +0100

    Add variables

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/Brick.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/Brick.java
@@ -1,8 +1,12 @@
 package uk.me.malpass.android_space_invaders_game;
 
+import android.graphics.RectF;
+
 /**
  * Created by sam on 08/04/2018.
  */
 
 public class Brick {
+    public RectF rect;
+    public boolean visibile;
 }
