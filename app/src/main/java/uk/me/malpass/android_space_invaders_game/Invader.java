bd2aee10b08b58069962ed213aaf700ad0320936
Sam <sam@malpass.me.uk>
2018-04-08 17:46:38 +0100

    Add variables

--- app/src/main/java/uk/me/malpass/android_space_invaders_game/Invader.java
+++ app/src/main/java/uk/me/malpass/android_space_invaders_game/Invader.java
@@ -10,4 +10,18 @@ import java.util.Random;
  */
 
 public abstract class Invader {
+    public RectF rect;
+    public Random generator = new Random();
+    public Bitmap bitmap1;
+    public Bitmap bitmap2;
+    public float length;
+    public float height;
+    public float x;
+    public float y;
+    public float speed;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+    public int moving = RIGHT;
+    public boolean visible;
+    public int lives;
 }
